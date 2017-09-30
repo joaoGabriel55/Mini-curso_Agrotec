a55fa5a71075a1302221de3c9586e416c89b30e6
Quaresma <j.quaresmasantos_98@hotmail.com>
2017-09-30 12:23:48 -0300

    Criado, classe modelo e DAO do modelo

--- Macaiba_market/src/model/Produto.java
+++ Macaiba_market/src/model/Produto.java
@@ -1,5 +1,159 @@
 package model;
 
-public class Produto {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produto")
+public class Produto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1020950800821773964L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer produtoId;
+
+	@Column(length=50, nullable = false)
+	private String nome;
+
+	@Column(length=50, nullable = false)
+	private String marca;
+
+	@Column(nullable = false)
+	private Double preco;
+
+	@Column(nullable = false)
+	private Double peso;
+
+	@Column(nullable = false)
+	private Integer quantidade;
+
+	public Produto(String nome, String marca, Double preco, Double peso, Integer quantidade) {
+		super();
+		this.nome = nome;
+		this.marca = marca;
+		this.preco = preco;
+		this.peso = peso;
+		this.quantidade = quantidade;
+	}
+
+	public Produto() {
+	}
+
+	public Integer getProdutoId() {
+		return produtoId;
+	}
+
+	public void setProdutoId(Integer produtoId) {
+		this.produtoId = produtoId;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((marca == null) ? 0 : marca.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((peso == null) ? 0 : peso.hashCode());
+		result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+		result = prime * result + ((produtoId == null) ? 0 : produtoId.hashCode());
+		result = prime * result + ((quantidade == null) ? 0 : quantidade.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (marca == null) {
+			if (other.marca != null)
+				return false;
+		} else if (!marca.equals(other.marca))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (peso == null) {
+			if (other.peso != null)
+				return false;
+		} else if (!peso.equals(other.peso))
+			return false;
+		if (preco == null) {
+			if (other.preco != null)
+				return false;
+		} else if (!preco.equals(other.preco))
+			return false;
+		if (produtoId == null) {
+			if (other.produtoId != null)
+				return false;
+		} else if (!produtoId.equals(other.produtoId))
+			return false;
+		if (quantidade == null) {
+			if (other.quantidade != null)
+				return false;
+		} else if (!quantidade.equals(other.quantidade))
+			return false;
+		return true;
+	}
 
 }
